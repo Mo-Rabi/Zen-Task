f27fe8697be6d3f04ccf32fd443a1f45a2de93a8
Mostafa-Rabea <mostafa.rapi3@gmail.com>
2023-10-27 22:47:28 +0200

    Fixed Deployment error

--- client/src/components/AddTask/AddTask.jsx
+++ client/src/components/AddTask/AddTask.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import styles from './AddTask.module.css'
+import styles from "./AddTask.module.css";
 
 export default function AddTask() {
-  return (
-    <div>AddTask</div>
-  )
+  return <div>AddTask</div>;
 }
